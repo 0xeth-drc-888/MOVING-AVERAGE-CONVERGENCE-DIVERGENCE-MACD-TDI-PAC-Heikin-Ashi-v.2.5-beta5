2a299cc6691582b137e88ad0d0f8aaa50a551d94
Daniel Isaac Toppan <70815142+Daniel-Toppan@users.noreply.github.com>
2020-09-16 12:58:02 -0400

    Add files via upload

---
+++ more_instructions.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  7 13:08:40 2020
+
+@author: user
+"""
+happy=1
+if happy > 2:
+    print('hello world')
+
+else:
+    print('sadboy')
+#If you are lower than a 5 for looks you are sad 
+    
+    
